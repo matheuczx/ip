6d45f7b68b7d8974f9d1bbdf93615ef63ce4450d
matheuczx <matheucheo16@gmail.com>
2026-01-29 17:16:23 +0800

    Revert "Added level 1 echo functionality"
    This reverts commit 94611af44c357e6932d7e808696e44275363a3a2.

--- src/main/java/HarryBotter.java
+++ src/main/java/HarryBotter.java
@@ -6,28 +6,21 @@ public class HarryBotter {
         System.out.println("Hello! I'm Harry, your loyal chatbot mate!");
         System.out.println("Oi! What can I do for you today? Type 'bye' when you're done, gov'nor.");
         System.out.println("____________________________________________________________");
-
         Scanner scanner = new Scanner(System.in);
         String input;
-        TaskManager manager = new TaskManager();
-
-
         while(true){
             input = scanner.nextLine();
-
-            if(input.equals("list")){
-                manager.listTasks();
-            }
-            else if(input.equals("bye")){
-                System.out.println("Cheers! I'm off to play some quiddich! Hope to see you lad again soon alright, mate!");
+            System.out.println("____________________________________________________________");
+            if(input.equals("bye")){
+                System.out.println("Cheerio! I'm off to play some quiddich! Hope to see you lad again soon alright, mate!");
                 System.out.println("____________________________________________________________");
                 break;
             }
             else{
-                manager.addTask(input);
-                System.out.println("Ah, you said: \"" + input + "\", added yo!");
+                System.out.println("Ah, you said: \"" + input + "\", righty-o!");
                 System.out.println("____________________________________________________________");
             }
+
         }
     }
 }
