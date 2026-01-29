94611af44c357e6932d7e808696e44275363a3a2
matheuczx <matheucheo16@gmail.com>
2026-01-29 17:12:40 +0800

    Added level 1 echo functionality
    Introduced British personality and switched to Harry Potter theme

---
+++ src/main/java/Task.java
@@ -0,0 +1,16 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description){
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
